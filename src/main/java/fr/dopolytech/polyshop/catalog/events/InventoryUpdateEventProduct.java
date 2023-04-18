312eed3abb3245d1a347ab1a0089401eb4079ea5
RedbeanGit <dubois.julien.mail@gmail.com>
2023-04-18 08:38:19 +0200

    fix: inventoryUpdateEvent bad success field
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- src/main/java/fr/dopolytech/polyshop/catalog/events/InventoryUpdateEventProduct.java
+++ src/main/java/fr/dopolytech/polyshop/catalog/events/InventoryUpdateEventProduct.java
@@ -2,7 +2,8 @@ package fr.dopolytech.polyshop.catalog.events;
 
 public class InventoryUpdateEventProduct {
     public String productId;
-    public int oldQuantity;
-    public int newQuantity;
-    public boolean isSuccessful;
+    public Integer oldQuantity;
+    public Integer newQuantity;
+    public Integer changeRequested;
+    public Boolean success;
 }
